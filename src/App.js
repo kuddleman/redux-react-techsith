12132b75915295d71b8c109d47f1679c345c788b
Kuddleman <edl1155@gmail.com>
2020-06-22 12:20:01 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    age: 21
+  }
+
+  onAgeUP = () => {
+    this.setState({
+      ...this.state,
+      age: ++this.state.age
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div>Age:<span>{ this.state.age }</span></div>
+        <button onClick={ this.onAgeUP }>Age UP</button>
+        <button>Age DOWN</button>
+      </div>
+    )
+  }
 }
 
 export default App;
